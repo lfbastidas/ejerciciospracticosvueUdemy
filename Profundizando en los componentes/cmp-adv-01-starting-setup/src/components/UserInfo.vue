6af92600c8a812ad5e5936eec4f8856f70482154
pipe444 <lfbastidas@unicauca.edu.co>
2021-03-30 12:15:42 -0500

    slots: envolver contenido dinamico

--- Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,11 +1,14 @@
 <template>
   <section>
+    <base-card>
     <div>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
     </div>
     <p>{{ infoText }}</p>
+    </base-card>
   </section>
+
 </template>
 
 <script>
@@ -13,19 +16,10 @@ export default {
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
-<!-- Estilos para este componente -->
+<!-- Estilos solo para este componente -->
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+
+
+
 </style>
