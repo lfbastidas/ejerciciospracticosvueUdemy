6a6259d87dbae404994ee4a40936a5b52a7d46fd
pipe444 <lfbastidas@unicauca.edu.co>
2021-03-30 11:37:04 -0500

    componentes globales y locales

--- Profundizando en los componentes/cmp-adv-01-starting-setup/src/App.vue
+++ Profundizando en los componentes/cmp-adv-01-starting-setup/src/App.vue
@@ -11,7 +11,17 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+
 export default {
+  components: {
+   'the-header': TheHeader,
+   'user-info': UserInfo,
+   'badge-list': BadgeList,
+
+  },
   data() {
     return {
       activeUser: {
