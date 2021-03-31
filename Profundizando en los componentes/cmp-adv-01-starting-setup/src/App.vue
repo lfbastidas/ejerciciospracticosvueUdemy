33b10f87ea2c446b79a8dc2d8ec3bba33be66b4f
pipe444 <lfbastidas@unicauca.edu.co>
2021-03-30 22:16:05 -0500

    Componentes dinamicos haciendo uso de IS

--- Profundizando en los componentes/cmp-adv-01-starting-setup/src/App.vue
+++ Profundizando en los componentes/cmp-adv-01-starting-setup/src/App.vue
@@ -1,37 +1,32 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
-      <template v-slot:default="slotProps">  <!-- slot Props me vincula las propiedades utilizadas en el slot usado en el componente CourseGoals -->
-      <h2>{{slotProps.item}}</h2>
-      <p>{{slotProps['another-prop']}} </p>
-      </template > 
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')"> Active Goals </button>
+    <button @click="setSelectedComponent('manage-goals')"> Manage Goals </button>
+  <!--  <active-goals v-if="selectedComponent ==='active-goals'"> </active-goals>
+    <manage-goals v-if="selectedComponent ==='manage-goals'"/> -->
+   <component :is="selectedComponent"> </component>
+
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
-import BadgeList from './components/BadgeList.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue'
 
 export default {
   components: {
    'the-header': TheHeader,
-   'user-info': UserInfo,
-   'badge-list': BadgeList,
-   'course-goals': CourseGoals
+   'active-goals': ActiveGoals,
+   'manage-goals': ManageGoals
+
+   
 
   },
   data() {
     return {
+      selectedComponent: 'active-goals',          //componente dinamico nombro como el componente
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -39,6 +34,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp){
+      this.selectedComponent=cmp;
+    }
+  }
 };
 </script>
 <!-- Aqui defino los estilos q usara la aplicacion a nivel global -->
