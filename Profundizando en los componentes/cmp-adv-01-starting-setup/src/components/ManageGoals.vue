38a18fa1b35b86a3da1f865868482696cdbca75b
pipe444 <lfbastidas@unicauca.edu.co>
2021-03-30 23:04:15 -0500

    Aplicando conceptos

--- Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,39 @@
 <template>
 <div>
     <h2> Manage Goals </h2>
-    <input type="text" />
+    <input type="text" v-model="enterdValue"> 
+    <h3> {{enterdValue}} </h3>
+    <button @click="setGoal"> Set Goal </button>
+    <error-alert v-if="inputIsInvalid">
+        <h2> Input is invalid </h2>
+        <p> Please enter at least a few characteres... </p>
+        <button @click="confirmError"> Okay </button>
+   </error-alert>
 </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+    components: {
+    ErrorAlert
+    },
+    data(){
+        return {
+            enterdValue:'',
+            inputIsInvalid: false
+        }
+    },
+    methods:{
+        setGoal(){
+            if (this.enterdValue ===''){
+               // alert('Input must not be empty')
+               this.inputIsInvalid=true;
+            }
+        },
+        confirmError(){
+            this.inputIsInvalid=false;
+        }
+    }
+}
+</script>
