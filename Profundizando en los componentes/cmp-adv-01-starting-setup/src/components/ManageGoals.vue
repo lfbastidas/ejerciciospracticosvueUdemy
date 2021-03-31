ec21510e47050390c28f358bd1f0bd6648aba737
pipe444 <lfbastidas@unicauca.edu.co>
2021-03-30 23:13:01 -0500

    uso de teleport para estructurar mejor el html

--- Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Profundizando en los componentes/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -4,11 +4,13 @@
     <input type="text" v-model="enterdValue"> 
     <h3> {{enterdValue}} </h3>
     <button @click="setGoal"> Set Goal </button>
+    <teleport to="body">
     <error-alert v-if="inputIsInvalid">
         <h2> Input is invalid </h2>
         <p> Please enter at least a few characteres... </p>
         <button @click="confirmError"> Okay </button>
    </error-alert>
+   </teleport>
 </div>
 </template>
 
