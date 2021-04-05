8e6b3a36a47fafc0875d6ff42af88bb8f76c8680
pipe444 <lfbastidas@unicauca.edu.co>
2021-04-05 11:50:52 -0500

    ingresando datos de padre a hijo y de hijo a padre, uso de provide e inject, enviando informacion de un form, haciendo uso de keep-alive pa q no se borren datos del form

--- Seccion 10-Course Project/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ Seccion 10-Course Project/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,3 +1,76 @@
 <template>
-    <h2> Add Resource </h2>
+    <base-card> 
+    <form @submit.prevent="submitData">
+        <div class="form-control">
+            <label for="title" > Title </label>
+            <input id="title" name="title" type="text" v-model="titleInput">
+        </div>
+        <div class="form-control">
+            <label for="description" > Description </label>
+            <textarea id="description" name="descirption" rows="3" v-model="descInput"> </textarea>
+            
+        </div>
+
+        <div class="form-control">
+            <label for="link" > Link</label>
+            <input id="link" name="link" type="url" v-model="linkInput">
+        </div>
+
+        <div class="form-control">
+            <the-button type="submit"> Add Resource </the-button>
+        </div>
+        
+    </form>
+    
+    </base-card>
 </template>
+<script>
+export default {
+  inject:['addResource']
+,  data () {
+    return {
+     titleInput:'',
+     descInput:"",
+     linkInput:"",
+    }
+  },
+  methods:  {
+  submitData(){
+
+const enterdTittle =  this.titleInput;
+const enterdDesc = this.descInput;
+const enterdLink = this.linkInput;
+
+this.addResource(enterdTittle, enterdDesc, enterdLink)
+  }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
--- Seccion 10-Course Project/prj-cmp-01-starting-setup/src/components/learning-resources/TheResource.vue
+++ Seccion 10-Course Project/prj-cmp-01-starting-setup/src/components/learning-resources/TheResource.vue
@@ -4,7 +4,9 @@
     <the-button @click="setSelectTab('store-resources')" :mode="storedResButtonMode" > Stored Resources </the-button>
     <the-button @click="setSelectTab('add-resource')" :mode="addResButtonMode"> Add Resources</the-button>
     </base-card>
+    <keep-alive>
     <component :is="selectTab"> </component>
+    </keep-alive>
 </div>
 </template>
 
@@ -49,20 +51,26 @@ export default {
 
 provide() {
     return {
-        resources: this.storedResources
+        resources: this.storedResources,
+        addResource: this.addResource
     }
 },
 
-
-
-
-
-
-    
-    methods: {
+   methods: {
         setSelectTab(tab){
             this.selectTab=tab;     
                     // lo hago para cambiar de pestañas
+        },
+        addResource (title, description, url) {
+            const newResource =  {
+                id: new Date().toISOString(),
+                tittle:title,
+                description: description,
+                link: url
+            }
+            this.storedResources.unshift(newResource);
+            this.selectTab = 'store-resources'
+
         }
     }
 }
