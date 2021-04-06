8e843fd718563ab2430515ec792a14dc2211c8b3
pipe444 <lfbastidas@unicauca.edu.co>
2021-04-06 18:33:23 -0500

    validacions de formularios

--- Forms-Seccion 11/src/components/TheForm.vue
+++ Forms-Seccion 11/src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity ==='invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if="userNameValidity==='invalid'">Please enter a valid Name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -70,7 +71,8 @@ export default {
       referrer: 'wom',   //obtener valores de un menu desplegable
       interest: [], // obtener datos de un checkbox
       how: null,
-      confirm:false
+      confirm:false,
+      userNameValidity: 'pending'
 
   }},
   methods: {
@@ -82,6 +84,13 @@ export default {
        console.log(this.how)
        this.interest = []
        this.how = null
+    },
+    validateInput(){
+      if(this.userName===''){
+this.userNameValidity='invalid'
+      }else {
+        this.userNameValidity='valid'
+      }
     }
   }
 }
@@ -101,6 +110,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+
+}
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
